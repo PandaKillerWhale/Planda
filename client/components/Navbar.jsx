60d14983dd0d5882e076ee2e79e342fcc023bf0b
Wei Huang <weimpromptu@gmail.com>
2021-01-21 14:06:49 -0500

    progressBar 1.2

--- client/components/Navbar.jsx
+++ client/components/Navbar.jsx
@@ -7,46 +7,39 @@ import LoginContainer from './LoginContainer';
 import ProgressBar from './ProgressBar.jsx';
 import CurrentGroupDisplay from './CurrentGroupDisplay.jsx';
 
-const progressData = [
-  { backgroundColor: "#FDDAD3", bgcolor: "#90D14F", barCat: "FrontEnd", completed: 25,  },
-  { backgroundColor: "#FFFDDA", bgcolor: "#90D14F", barCat: "FrontEnd_UX", completed: 55 },
-  { backgroundColor: "#EAF6DC", bgcolor: "#90D14F", barCat: "BackEnd", completed: 53 },
-  { backgroundColor: "#D0F5F6", bgcolor: "#90D14F", barCat: "Data_Base", completed: 75 },
-];
-
 const Navbar = () => {
 
   const [taskCategories, setTaskCategories] = useState(['AppConfig', 'Webpack', 'Backend', 'Frontend']);
   const [currentTaskCat, toggleCurrentTask] = useState('');
   const [cards, setCards] = useState([]);
-  const [userState, setUserState] = useState({name:'', id:'', groups:[], group_id:[], enabled:false});
+  const [userState, setUserState] = useState({ name: '', id: '', groups: [], group_id: [], enabled: false });
   const [currentDisplay, setCurrentDisplay] = useState(userState.name);
 
   /* on page load, we pull the data for the current Task Cat,  */
   useEffect(() => {
-    fetch('/api/user/groups')  
+    fetch('/api/user/groups')
       .then(res => res.json())
       .then(data => {
-        const newUser = {name:data.username,id:data.id, groups:[], group_id:[]};
-        data.userGroups.forEach( element => (newUser.groups.push(element.name), newUser.group_id.push(element.group_id)));
+        const newUser = { name: data.username, id: data.id, groups: [], group_id: [] };
+        data.userGroups.forEach(element => (newUser.groups.push(element.name), newUser.group_id.push(element.group_id)));
         setUserState(newUser);
       });
   }, []);
 
   useEffect(() => {
     if (userState.groups.indexOf(currentDisplay) >= 0) {
-    fetch('/api/group/cards/'+userState.group_id[userState.groups.indexOf(currentDisplay)])  // "/api/group/cards/""
-      .then(res => res.json())
-      .then(data => {
-        setCards(data);
-      });
+      fetch('/api/group/cards/' + userState.group_id[userState.groups.indexOf(currentDisplay)])  // "/api/group/cards/""
+        .then(res => res.json())
+        .then(data => {
+          setCards(data);
+        });
     } else if (currentDisplay === userState.name) {
       fetch('/api/user/cards/')  // "/api/group/cards/""
-      .then(res => res.json())
-      .then(data => {
-        console.log(data)
-        setCards(data);
-      });
+        .then(res => res.json())
+        .then(data => {
+          console.log(data)
+          setCards(data);
+        });
     }
   }, [currentDisplay])
 
@@ -54,7 +47,7 @@ const Navbar = () => {
     return toggleCurrentTask(e.target.id);
   }
 
-  
+
   const darkLightMode = () => {
     const body = document.body;
     body.classList.toggle("dark-mode")
@@ -65,26 +58,26 @@ const Navbar = () => {
   taskCategories.forEach((category, index) => {
     catButtons.push(<div>
       <h1 id={category} onClick={taskChecker}>
-        <TaskCategory key={`button${category}${index}`} type={category}/>
+        <TaskCategory key={`button${category}${index}`} type={category} />
       </h1>
     </div>)
   });
 
-  const switchUserPanel = () => {return userState.enabled ? setUserState({...userState, enabled: false}) : setUserState({...userState, enabled: true})}
+  const switchUserPanel = () => { return userState.enabled ? setUserState({ ...userState, enabled: false }) : setUserState({ ...userState, enabled: true }) }
   const currentTaskShow = [];
-    if (currentTaskCat.length > 1) {
-      currentTaskShow.push(<div className="grid-container">
-        <TaskCategorySteps key={`Steps${currentTaskCat}`}  type={currentTaskCat} cardData={cards} userData={userState} />
-      </div>)
-    }
+  if (currentTaskCat.length > 1) {
+    currentTaskShow.push(<div className="grid-container">
+      <TaskCategorySteps key={`Steps${currentTaskCat}`} type={currentTaskCat} cardData={cards} userData={userState} />
+    </div>)
+  }
 
-    // USER PANEL ENABLER 
-    const userPanel = [];
-    if (userState.enabled) userPanel.push(<UserPanel key='UserPanel1' userState={userState} setCurrentDisplay={setCurrentDisplay} setUserState={setUserState}/>)
-    // LOGIN ENABLER
-    const login = [];
-  if (!userState.name) login.push(<LoginContainer key='LoginContainer1' setUser={setUserState}/>)
-  
+  // USER PANEL ENABLER 
+  const userPanel = [];
+  if (userState.enabled) userPanel.push(<UserPanel key='UserPanel1' userState={userState} setCurrentDisplay={setCurrentDisplay} setUserState={setUserState} />)
+  // LOGIN ENABLER
+  const login = [];
+  if (!userState.name) login.push(<LoginContainer key='LoginContainer1' setUser={setUserState} />)
+
   const getGroupIdFromName = (name) => {
     // if name is not set
     if (!name) return;
@@ -94,9 +87,46 @@ const Navbar = () => {
     return userState.group_id[index];
   };
 
+  /* PROGRESS BAR FUNCTION */
+
+  /* 2. PROGRESS OVERALL CARDS FUNCTION :: MAIN PROGRESS BAR */
+  // const overAllCards = [];
+  // const cardsFinished = [];
+
+  // for (let i = 0; i < cards.length; i++) {
+  //   overAllCards.push(cards[i]);
+  //   if (cards[i].status === 1) {
+  //     cardsFinished.push(cards);
+  //   }
+  // }
+
+  const progressData = [
+    { backgroundColor: "#FDDAD3", bgcolor: "#90D14F", barCat: "FrontEnd", completed: 25, status: 0 },
+    { backgroundColor: "#FFFDDA", bgcolor: "#90D14F", barCat: "FrontEnd_UX", completed: 55, status: 0 },
+    { backgroundColor: "#EAF6DC", bgcolor: "#90D14F", barCat: "BackEnd", completed: 53, status: 1 },
+    { backgroundColor: "#D0F5F6", bgcolor: "#90D14F", barCat: "Data_Base", completed: 75, status: 1 },
+  ];
+
+  /* 1. PROGRESS BAR FUNCTION :: INDIVIDUAL PROGRESS BAR PER BOARD */
+  // const progressBar = [];
+  // const boardCards = cards.filter(card => );
+  // console.log(`sample cards detail : ${cards[0]}`);
+
+  // progressData.map((item, idx) => {
+  //   progressBar.push(
+  //     <div className="progressbar">
+  //       <ProgressBar key={idx} backgroundColor={item.backgroundColor} bgcolor={item.bgcolor} barCat={item.barCat} completed={item.completed} />
+  //     </div>
+  //   )
+  // });
+  // console.log(`overAllCards numer = ${overAllCards.length}`);
+  // console.log(`overAllCards = ${cardsFinished.length}`);
+  // /* PROGRESS BAR FUNCTION */
+
+  // GROUP ICON DIVS AND PROGRESSBAR
   return (
     <div>
-     <div id='Icons'><img  id='theUserIcon' onClick={switchUserPanel} /><img id='DarkLightIcon'  onClick={darkLightMode} /></div>
+      <div id='Icons'><img id='theUserIcon' onClick={switchUserPanel} /><img id='DarkLightIcon' onClick={darkLightMode} /></div>
       <CurrentGroupDisplay
         name={currentDisplay}
         id={getGroupIdFromName(currentDisplay)}
@@ -114,12 +144,11 @@ const Navbar = () => {
           <ProgressBar key={idx} backgroundColor={item.backgroundColor} bgcolor={item.bgcolor} barCat={item.barCat} completed={item.completed} />
         ))}
       </div>
-        {currentTaskShow}
+      {currentTaskShow}
     </div>
   )
 }
 
 export default Navbar;
-
 
 // src='client/assets/light_dark.png'
--- client/components/ProgressBar.jsx
+++ client/components/ProgressBar.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const ProgressBar = (props) => {
   const { bgcolor, barCat, completed } = props;
   // styling will move to style.css when feature finish mockup
- 
+
   const fillerStyles = {
     height: '100%',
     width: `${completed}%`,
@@ -12,25 +12,17 @@ const ProgressBar = (props) => {
     borderRadius: 'inherit',
     textAlign: 'right'
   }
- 
-  // inline styling for progress bar
- /*  return (
-    <div className="barContainerStyles">
-      <div style={fillerStyles}>
-        <span style={labelStyles}>{`${barCat} ${completed}%`}</span>
-      </div>
-    </div>
-  ); */
+
+
 
   return (
     <div className="barContainerStyles">
       <div style={fillerStyles}>
-        <span className="labelStyles">{`${barCat} ${completed}%`}</span>
+        <span className="labelStyles">{`${completed}%`}</span>
       </div>
     </div>
   );
 };
 
-export default ProgressBar; 
+export default ProgressBar;
 
- 
